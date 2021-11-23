6af898b7c0a5193417859d8981bb74b6b9a49458
Noa <noasueden@gmail.com>
2021-11-23 14:25:03 +0900

    Day2 - Question1 to 5

---
+++ src/java/Day2/q1.java
@@ -0,0 +1,18 @@
+package java.Day2;
+
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if(A>B){
+            System.out.println(">");
+        }else if(A<B){
+            System.out.println("<");
+        }else if(A==B){
+            System.out.println("==");
+        }
+    }
+}
---
+++ src/java/Day2/q3.java
@@ -0,0 +1,21 @@
+package java.Day2;
+
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(year%4==0){
+            if(year%400==0){
+                System.out.println("1");
+            }else if(year%100==0){
+                System.out.println("0");
+            }else{
+                System.out.println("1");
+            }
+        }else{
+            System.out.println("0");
+        }
+    }
+}
